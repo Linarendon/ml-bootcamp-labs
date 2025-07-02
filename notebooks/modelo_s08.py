1e447b0cd99aa4dec37ebf44b394ef7edbfc2938
Linilla <linam_rendon@yahoo.com>
2025-07-02 22:16:45 +0000

    Copiado de todos los archivos de Luis a las carpetas

--- notebooks/modelo_s08.py
+++ notebooks/modelo_s08.py
@@ -1,17 +1,24 @@
-# model08.py
-# #  **Preparación del Entorno
-# # Importar las librerías necesarias (Pandas, NumPy, Scikit-learn, Matplotlib/Seaborn, SHAP).
-# # Cargar un dataset y un modelo entrenado (se proporcionará un ejemplo, o se usará el resultado de sesiones anteriores).
+# %% [markdown]
+# # Sesión 06 - Manejo de Modelos de Clasificación para entornos operativos
+
+# %% [markdown]
+# *Objetivo*
+# 
+# - Aplicar algoritmos de clasificación para identificar eventos operacionales críticos en la industria petrolera.
+# - Evaluar el rendimiento de diferentes clasificadores.
+# - Interpretar los resultados y la matriz de confusión.
+# - Analizar la eficiencia en pozos productores.
 
 # %%
-!python --version
+#!python --version
 
-# %%
-# Instalación de dependencias del nuevo codespace
-!pip install -r ../requirements.txt
 
 # %% [markdown]
 # ## 0. Carga de Librerías
+
+# %%
+# Instalación de dependencias del nuevo codespace
+#!pip install -r ../requirements.txt
 
 # %%
 # Importación de Librerías
@@ -37,6 +44,7 @@ from sklearn.metrics import mean_absolute_error, root_mean_squared_error, mean_s
 # Ruta Absoluta en Linux o Mac
 df = pd.read_csv("/workspaces/ml-bootcamp-labs/data/raw/Operational_events.csv")
 
+
 # %%
 df
 
@@ -53,11 +61,20 @@ df.describe()
 # %%
 df.loc[ df.Date.isnull()  ]
 
+# %%
+df.Event_Type.hist()
+
 # %% [markdown]
 # ## 3. Transformaciones (Encoding, Imputación)
 
 # %%
 df.isnull().sum()
+
+# %%
+df.Temperature.describe()
+
+# %%
+df.Temperature.to_frame().boxplot()
 
 # %%
 # Método de Imputación "Simple"
@@ -67,7 +84,6 @@ df.Pressure.fillna(df.Pressure.mean())
 
 # %%
 from sklearn.preprocessing import LabelEncoder
-
 
 # %%
 label_encoder = LabelEncoder()
@@ -88,6 +104,8 @@ manual_encode = {
 
 df.Event_Type.map(manual_encode)
 
+# %%
+df.info()
 
 # %%
 # Instrucción para descartar columnas que no sean numéricas
@@ -95,14 +113,19 @@ df.Event_Type.map(manual_encode)
 df_encoded = df.select_dtypes(exclude=['object']).rename(columns={"Event_Type_n":"Event_Type"})
 
 # %%
+df_encoded
+
+# %%
 # Carga de librería de SimpleImputer
 from sklearn.impute import SimpleImputer
+
+# %%
+df_encoded.info()
 
 # %%
 imputer = SimpleImputer(strategy="mean")
 
 df_imputado = pd.DataFrame( imputer.fit_transform(df_encoded), columns= df_encoded.columns  )
-
 
 # %%
 """
@@ -113,6 +136,9 @@ Event_Type_n  Event_Type
 3             Pump Failure     25
 Name: count, dtype: int64
 """
+
+# %%
+df_imputado
 
 # %%
 df_imputado.Event_Type.value_counts()
@@ -142,12 +168,18 @@ minority_class_df_sample
 # %%
 df_balanced = pd.concat([ majority_class_df_sample, minority_class_df_sample])
 
+# %%
+df_balanced.Event_Type.value_counts()
+
 # %% [markdown]
 # ## 4. Modelamiento
 
 # %%
 # Carga de librerías para modelos
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
 
 # %%
 X = df_balanced.drop(columns=["Event_Type"])
@@ -158,11 +190,21 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, train_size= 0.8, random
 model_dtc = DecisionTreeClassifier()
 model_dtc.fit(X_train,y_train)
 
+# %%
+model_knc = KNeighborsClassifier()
+model_knc.fit(X_train,y_train)
+
+# %%
+model_svc = SVC()
+model_svc.fit(X_train,y_train)
+
 # %% [markdown]
 # ## 5. Evaluación
 
 # %%
 predict_dtc = model_dtc.predict(X_test)
+predict_knc = model_knc.predict(X_test)
+predict_svc = model_svc.predict(X_test)
 
 # %%
 # Evaluación DTC
@@ -181,10 +223,20 @@ print(dtc_f1)
 # %%
 dtc_cmatrix
 
-# %% [markdown]
-# ## 2.  **Cálculo de la Importancia de las Características (30 minutos)**
-# # *   Utilizar métodos inherentes al modelo (ej: `feature_importances_` en Árboles de Decisión).
-# # *   Implementar permutation importance (Scikit-learn).
-# # *   Introducción a SHAP (SHapley Additive exPlanations) para una interpretación más completa (opcional, si el tiempo lo permite).
+# %%
+dtc_cmatrix
+
+# %%
+confusion_matrix(y_pred= predict_knc, y_true= y_test)
+
+# %%
+confusion_matrix(y_pred= predict_svc, y_true= y_test)
 
 
+import streamlit as st
+
+st.title("Aplicación de clasificación")
+
+st.write("Modelo de clasificación con DTC")
+
+st.write(dtc_accuracy)
