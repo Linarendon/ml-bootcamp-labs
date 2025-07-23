d3bd0826f89f98f0fad71db0049412de26d03dd9
Linilla <linam_rendon@yahoo.com>
2025-07-23 21:34:35 +0000

    instalacion scikit-leran

--- script_sesion12.py
+++ script_sesion12.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
